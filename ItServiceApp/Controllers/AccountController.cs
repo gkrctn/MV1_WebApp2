07d52c910e36a174a52b42effa574e951101cbb7
gkrctn <gkr.cetin@gmail.com>
2021-12-23 18:17:17 +0300

    login&loguot actions ok

--- ItServiceApp/Controllers/AccountController.cs
+++ ItServiceApp/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ItServiceApp.Models.Identity;
+using ItServiceApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,86 @@ namespace ItServiceApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Register()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user!=null)
+            {
+                ModelState.AddModelError(nameof(model.UserName), "Bu kullanıcı adı daha önce kayıt edilmiştir.");
+                return View(model);
+            }
+            user = await _userManager.FindByEmailAsync(model.Email);
+            if (user!=null)
+            {
+                ModelState.AddModelError(nameof(model.Email), "Bu email adresi daha önce kayıt edilmiştir.");
+            }
+            user = new ApplicationUser()
+            {
+                Email = model.Email,
+                Name = model.Name,
+                UserName = model.UserName,
+                Surname = model.Surname
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                //kullanıcıya rol atama
+                //email onay maili
+                //login sayfasına yönlendirme
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Bir Hata oluştu");
+                return View(model);
+            }
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Kullanıcı adı veya şifre hatalı");
+                return View(model);
+            }
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
